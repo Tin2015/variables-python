e7fc2bd53aad9e6c5898cc2e1a6ee8dc246a1e22
JavierMarin <oscarjavimarin@gmail.com>
2023-07-26 21:12:40 -0500

    archivo finalizado

--- archivo.py
+++ archivo.py
@@ -2,4 +2,15 @@
 nombre = "Oscar Javier" #String
 edad = 36 #int
 estatura = 1.99 #float
-natación = false #booleano
+deporte_natación = false #booleano
+print("Primer punto: ")
+print(f"{nombre} , {edad} , {estatura} , {deporte_natacion}")
+
+#Los numeros enteros son aquellos que no tienen decimales, tanto positivos como negativos (ademas del 0). En phyton se pueden representar mediante el tipo int o el tipo long. La unica diferencia es que el tipo long permite almacenar numeros mas grandes.
+
+#Para convertir flotantes en enteros se usa la función int()
+print("Tercer punto: ")
+print(ingrese un numero para aplicar la formula de los primeros n numeros pares)
+n=int(input())
+suma-n*(n+)
+print(f"El resultado aplicando la formula de la suma de los primeros n numeros pares es: {suma}")
